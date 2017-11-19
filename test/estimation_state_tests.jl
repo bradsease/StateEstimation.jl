fb6ac370da533b8e18ffaa3679de35d5f1cadcae
Brad Sease <bsease@vt.edu>
2017-11-19 16:13:16 -0500

    Added state tests.

--- test/estimation_state_tests.jl
+++ test/estimation_state_tests.jl
@@ -5,6 +5,56 @@ using Base.Test
 @test 1 == 1
 
 
+# Discrete state constructors
+@test size(DiscreteState(1.0).x) == (1,)
+@test size(DiscreteState(1.0, 0).x) == (1,)
+@test size(UncertainDiscreteState(1.0, 2.0).x) == (1,)
+@test size(UncertainDiscreteState(1.0, 2.0, 0).P) == (1, 1)
+@test DiscreteState(1.0) == DiscreteState(UncertainDiscreteState(1.0, 2.0))
+@test UncertainDiscreteState(ones(2), eye(2)) ==
+    UncertainDiscreteState(DiscreteState(ones(2)), eye(2))
+
+# Continuous state constructors
+@test size(ContinuousState(1.0).x) == (1,)
+@test size(ContinuousState(1.0, 0.0).x) == (1,)
+@test size(UncertainContinuousState(1.0, 2.0).x) == (1,)
+@test size(UncertainContinuousState(1.0, 2.0, 0.0).P) == (1, 1)
+@test ContinuousState(1.0) == ContinuousState(UncertainContinuousState(1.0,2.0))
+@test UncertainContinuousState(ones(2), eye(2)) ==
+    UncertainContinuousState(ContinuousState(ones(2)), eye(2))
+
+# Create testing states
+discrete_state = DiscreteState(ones(2))
+discrete_state = DiscreteState(ones(2), 0)
+unc_discrete_state = UncertainDiscreteState(ones(2), eye(2))
+unc_discrete_state = UncertainDiscreteState(ones(2), eye(2), 0)
+continuous_state = ContinuousState(ones(2))
+continuous_state = ContinuousState(ones(2), 0.0)
+unc_continuous_state = UncertainContinuousState(ones(2), eye(2))
+unc_continuous_state = UncertainContinuousState(ones(2), eye(2), 0.0)
+
+# Test type conversions
+@test StateEstimation.absolute_type(unc_discrete_state) <: DiscreteState
+@test StateEstimation.absolute_type(unc_continuous_state) <: ContinuousState
+@test StateEstimation.uncertain_type(discrete_state) <: UncertainDiscreteState
+@test StateEstimation.uncertain_type(continuous_state) <: UncertainContinuousState
+@test discrete_state == make_absolute(unc_discrete_state)
+@test unc_discrete_state == make_uncertain(discrete_state, unc_discrete_state.P)
+@test continuous_state == make_absolute(unc_continuous_state)
+@test unc_continuous_state == make_uncertain(continuous_state,
+                                             unc_continuous_state.P)
+
+# Sample uncertain states
+@test typeof(sample(unc_discrete_state)) <: DiscreteState
+@test typeof(sample(unc_continuous_state)) <: ContinuousState
+
+# Test distance metrics
+@test distance(discrete_state, discrete_state) == 0
+@test distance(discrete_state, unc_discrete_state) == 0
+@test mahalanobis(discrete_state, unc_continuous_state) == 0
+@test mahalanobis(DiscreteState([4.0, 1.0]), unc_discrete_state) == 3
+@test mahalanobis(discrete_state, unc_discrete_state) ==
+    mahalanobis(discrete_state, discrete_state, unc_discrete_state.P)
 
 function test_state_addition_subtraction(state::AbstractState)
     for a = -1:0.5:1
@@ -32,12 +82,16 @@ function test_state_scalar_multiplication(state::AbstractState)
         new_state = deepcopy(state)
         new_state .*= a
         @test new_state.x == (state.x*a)
+        new_state .= a .* new_state
+        @test new_state.x == (state.x*a^2)
         if typeof(state) <: AbstractUncertainState
             @test (state*a).P == (state.P*a^2)
             @test (a*state).P == (state.P*a^2)
             new_state = deepcopy(state)
             new_state .*= a
             @test new_state.P == (state.P*a^2)
+            new_state .= a .* new_state
+            @test new_state.P == (state.P*a^4)
         end
     end
 end
@@ -50,62 +104,9 @@ test_state_scalar_multiplication(UncertainContinuousState(ones(2), eye(2)))
 @test DiscreteState(ones(2)) == DiscreteState([1.0, 1.0])
 @test UncertainDiscreteState(ones(2), eye(2)) ==
     UncertainDiscreteState([1.0, 1.0], eye(2))
-@test UncertainDiscreteState(ones(2), eye(2)) != DiscreteState(ones(2))
-@test DiscreteState(ones(2)) != ContinuousState(ones(2))
+@test (UncertainDiscreteState(ones(2), eye(2))==DiscreteState(ones(2))) == false
+@test (DiscreteState(ones(2)) == ContinuousState(ones(2))) == false
 @test ContinuousState(ones(2)) == ContinuousState([1.0, 1.0])
 @test UncertainContinuousState(ones(2), eye(2)) ==
     UncertainContinuousState([1.0, 1.0], eye(2))
 @test UncertainContinuousState(ones(2), eye(2)) != ContinuousState(ones(2))
-
-# Create discrete states
-@test size(DiscreteState(1.0).x) == (1,)
-@test size(UncertainDiscreteState(1.0, 2.0).x) == (1,)
-@test size(UncertainDiscreteState(1.0, 2.0).P) == (1, 1)
-@test DiscreteState(1.0) == DiscreteState(UncertainDiscreteState(1.0, 2.0))
-#
-
-# Create continuous states
-@test size(ContinuousState(1.0).x) == (1,)
-@test size(UncertainContinuousState(1.0, 2.0).x) == (1,)
-@test size(UncertainContinuousState(1.0, 2.0).P) == (1, 1)
-@test ContinuousState(1.0) == ContinuousState(UncertainContinuousState(1.0,2.0))
-
-# Create states
-discrete_state = DiscreteState(ones(2))
-discrete_state = DiscreteState(ones(2), 0)
-unc_discrete_state = UncertainDiscreteState(ones(2), eye(2))
-unc_discrete_state = UncertainDiscreteState(ones(2), eye(2), 0)
-continuous_state = ContinuousState(ones(2))
-continuous_state = ContinuousState(ones(2), 0.0)
-unc_continuous_state = UncertainContinuousState(ones(2), eye(2))
-unc_continuous_state = UncertainContinuousState(ones(2), eye(2), 0.0)
-
-# Convert state types with constructors
-@inferred DiscreteState(unc_discrete_state)
-@inferred UncertainDiscreteState(discrete_state)
-@inferred UncertainDiscreteState(discrete_state, eye(2))
-@inferred ContinuousState(unc_continuous_state)
-@inferred UncertainContinuousState(continuous_state)
-@inferred UncertainContinuousState(continuous_state, eye(2))
-
-# Convert state types with general methods
-@inferred make_absolute(unc_continuous_state)
-@inferred make_absolute(unc_discrete_state)
-@inferred make_uncertain(continuous_state)
-@inferred make_uncertain(continuous_state, eye(2))
-@inferred make_uncertain(discrete_state)
-@inferred make_uncertain(discrete_state, eye(2))
-
-# Sample uncertain states
-@inferred sample(unc_discrete_state)
-@inferred sample(unc_continuous_state)
-
-# Test distance metrics
-@test distance(discrete_state, discrete_state) == 0
-@test distance(discrete_state, unc_discrete_state) == 0
-@test mahalanobis(discrete_state, unc_continuous_state) == 0
-@test mahalanobis(DiscreteState([4.0, 1.0]), unc_discrete_state) == 3
-@test mahalanobis(discrete_state, unc_discrete_state) ==
-    mahalanobis(discrete_state, discrete_state, unc_discrete_state.P)
-@inferred distance(discrete_state, discrete_state)
-@inferred mahalanobis(discrete_state, unc_continuous_state)
