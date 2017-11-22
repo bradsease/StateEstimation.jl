3eabc6087f497cd6f8248ba57411686c62862c8c
Brad Sease <bsease@vt.edu>
2017-11-22 17:47:19 -0500

    Adding kalman_filter.jl tests.

--- test/kalman_filter_tests.jl
+++ test/kalman_filter_tests.jl
@@ -1,34 +1,48 @@
 using StateEstimation
 using Base.Test
 
-# Sanity check
-@test 1 == 1
-
-
-# Create linear observer
+# Test discrete kalman filter
+srand(1);
 linear_sys = LinearSystem(0.5*eye(2), 0.001*eye(2))
 linear_obs = LinearObserver(eye(2), 0.001*eye(2))
 initial_est = UncertainDiscreteState([1.0, 2.0], 0.1*eye(2))
 kf = KalmanFilter(linear_sys, linear_obs, initial_est)
 for i = 1:10
-    #println(kf.estimate)
     measurement = simulate(kf)
     process!(kf, measurement)
 end
-#println(kf.estimate)
 
-#println("***")
-#println("***")
+# Test discrete kalman filter with archiving
+srand(1);
+linear_sys = LinearSystem(0.5*eye(2), 0.001*eye(2))
+linear_obs = LinearObserver(eye(2), 0.001*eye(2))
+initial_est = UncertainDiscreteState([1.0, 2.0], 0.1*eye(2))
+kf = KalmanFilter(linear_sys, linear_obs, initial_est)
+archive = EstimatorHistory()
+for i = 1:10
+    measurement = simulate(kf)
+    process!(kf, measurement, archive)
+end
 
-# Create linear observer
+# Test continuous kalman filter
+srand(1);
 linear_sys = LinearSystem(-eye(2), 0.001*eye(2))
 linear_obs = LinearObserver(eye(2), 0.001*eye(2))
 initial_est = UncertainContinuousState([1.0, 2.0], 0.1*eye(2))
 kf = KalmanFilter(linear_sys, linear_obs, initial_est)
-arc = EstimatorHistory()
 for i = 1:10
-    #println(kf.estimate)
     measurement = simulate(kf, kf.estimate.t+0.1)
-    process!(kf, measurement, arc)
+    process!(kf, measurement)
 end
-#println(kf.estimate)
+
+# Test continuous kalman filter with archiving
+srand(1);
+linear_sys = LinearSystem(-eye(2), 0.001*eye(2))
+linear_obs = LinearObserver(eye(2), 0.001*eye(2))
+initial_est = UncertainContinuousState([1.0, 2.0], 0.1*eye(2))
+kf = KalmanFilter(linear_sys, linear_obs, initial_est)
+archive = EstimatorHistory()
+for i = 1:10
+    measurement = simulate(kf, kf.estimate.t+0.1)
+    process!(kf, measurement, archive)
+end
